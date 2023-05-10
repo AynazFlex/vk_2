989905a91372964703b38da1c810dd4c81602e3a
AynazFlex <jkz1713x2@gmail.com>
2023-05-10 22:15:06 +0300

    add date and time

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ function App() {
   const [floor, setFloor] = useState("");
   const [conference_room, setConference_room] = useState("");
   const [comment, setComment] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(tower && floor && conference_room && comment) {
-      console.log(JSON.stringify({tower, floor, conference_room, comment}))
+    if (tower && floor && conference_room && comment && date && time) {
+      console.log(
+        JSON.stringify({ tower, floor, conference_room, comment, date, time })
+      );
     } else {
-      alert("Заполните все поля")
+      alert("Заполните все поля");
     }
-  }
+  };
 
   const handleReset = () => {
-    setTower('')
-    setFloor('')
-    setConference_room('')
-    setComment('')
-  }
+    setTower("");
+    setFloor("");
+    setConference_room("");
+    setComment("");
+    setDate("");
+    setTime("")
+  };
 
   return (
     <div className={app.app}>
@@ -37,31 +43,10 @@ function App() {
           value={floor}
           setValue={(value) => setFloor(value)}
           items={[
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "11",
-            "12",
-            "13",
-            "14",
-            "15",
-            "16",
-            "17",
-            "18",
-            "19",
-            "20",
-            "21",
-            "22",
-            "23",
-            "24",
-            "25",
-            "26",
-            "27",
+            "3", "4", "5", "6", "7", "8", "9",
+            "10", "11", "12", "13", "14", "15",
+            "16", "17", "18", "19", "20", "21",
+            "22", "23", "24", "25", "26", "27",
           ]}
           placeholder="Веберите этаж"
         />
@@ -71,14 +56,26 @@ function App() {
           items={["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]}
           placeholder="Веберите переговорку"
         />
+        <input
+          className={app.date}
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <input
+          className={app.time}
+          type="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
         <textarea
           className={app.comment}
           value={comment}
           onChange={(e) => setComment(e.target.value)}
           placeholder="Напишите комментарий"
         />
-        <input className={app.submit} type="submit" value="Отправить"/>
-        <input className={app.reset} type="reset" value="Очистить"/>
+        <input className={app.submit} type="submit" value="Отправить" />
+        <input className={app.reset} type="reset" value="Очистить" />
       </form>
     </div>
   );
